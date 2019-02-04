f2932e48edc8bde3583fbeb026e755fe35bf049f
Martín López <mlopez@flowingcode.com>
2019-02-04 18:26:21 -0300

    Enhanced the DemoView, by using cards addon

--- src/test/java/com/flowingcode/vaadin/addons/rssitems/DemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/rssitems/DemoView.java
@@ -1,27 +1,7 @@
 package com.flowingcode.vaadin.addons.rssitems;
 
-/*-
- * #%L
- * RSS Items
- * %%
- * Copyright (C) 2019 Flowing Code
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
-import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.html.Div;
+import com.github.appreciated.card.Card;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -29,16 +9,19 @@ import com.vaadin.flow.router.Route;
  */
 @Route("")
 @SuppressWarnings("serial")
-public class DemoView extends Div {
+public class DemoView extends FlexLayout {
 	
 	public DemoView() {
-		Dialog dialog = new Dialog();
-		RssItems items = new RssItems("https://www.flowingcode.com/feeds/posts/default?alt=rss",6,100,100,true);
-		dialog.add(items);
-
-		dialog.setWidth("700px");
-		dialog.setHeight("400px");
-		dialog.open();
+		setSizeFull();
+		setJustifyContentMode(JustifyContentMode.CENTER);
+		Card card = new Card(new RssItems("https://www.flowingcode.com/feeds/posts/default?alt=rss",6,100,100,true));
+		card.setWidth("90vw");
+		card.getContent().setPadding(true);
+		card.getContent().getStyle().set("height", "90vh");
+		card.getContent().getStyle().set("overflow-y", "scroll");
+		add(card);
+		setAlignSelf(Alignment.CENTER, card);
 	}
+	
 	
 }
