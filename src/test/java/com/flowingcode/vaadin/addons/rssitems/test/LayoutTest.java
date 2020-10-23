ecc7ac3f6ddf132494ed1fa32b442d79d9607193
Nicolas Gonzalez Paz <ngonzalez@flowingcode.com>
2020-10-23 09:27:53 -0300

    Added demo-layout

--- src/test/java/com/flowingcode/vaadin/addons/rssitems/test/LayoutTest.java
+++ src/test/java/com/flowingcode/vaadin/addons/rssitems/test/LayoutTest.java
@@ -17,20 +17,24 @@
  * limitations under the License.
  * #L%
  */
-package com.flowingcode.vaadin.addons.rssitems;
+package com.flowingcode.vaadin.addons.rssitems.test;
 
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+import com.flowingcode.vaadin.addons.DemoLayout;
+import com.flowingcode.vaadin.addons.rssitems.RssitemsDemoView;
 import com.vaadin.flow.router.Route;
 
-@SuppressWarnings("serial")
-@Route("")
-public class DemoView extends VerticalLayout implements BeforeEnterObserver {
+public class LayoutTest {
 
-	@Override
-	public void beforeEnter(BeforeEnterEvent event) {
-		event.forwardTo(RssitemsDemoView.class);
+	@Test
+	public void testDemoLayout() {
+		Route route = RssitemsDemoView.class.getAnnotation(Route.class);
+		assertEquals("com.flowingcode.vaadin.addons.DemoLayout",DemoLayout.class.getName());
+		assertEquals(DemoLayout.class, route.layout());
+		assertNotEquals("", route.value());
 	}
-
 }
